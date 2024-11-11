e0b80cd52e486ba90d93697426d3ec7614d6082a
vijaisuria <98590895+vijaisuria@users.noreply.github.com>
2024-11-12 00:31:19 +0530

    Updated logo, event clickable

--- src/components/EventsSection/index.js
+++ src/components/EventsSection/index.js
@@ -1,60 +1,76 @@
 import React, { useState } from "react";
-import { events } from "../../shared/data"; 
+import { useNavigate } from "react-router-dom";
+import { events } from "../../shared/data";
 import styles from "./styles.module.scss";
 
 const EventsSection = () => {
-    const [selectedCategory, setSelectedCategory] = useState("Technical");
-    const categories = ["Technical", "Non Technical", "Fun Tech", "Workshop"];
+  const navigate = useNavigate();
 
-    const filteredEvents = events.filter(event => event.tags.includes(selectedCategory));
+  const [selectedCategory, setSelectedCategory] = useState("Technical");
+  const categories = ["Technical", "Non Technical", "Fun Tech", "Workshop"];
 
-    const handleCategoryChange = (category) => {
-        setSelectedCategory(category);
-    };
+  const filteredEvents = events.filter((event) =>
+    event.tags.includes(selectedCategory)
+  );
 
-    return (
-        <div className={styles.events_section} id="events">
-            <h1>EVENTS</h1>
-            
-            <div className={styles.navbar}>
-                {categories.map(category => (
-                    <button
-                        key={category}
-                        className={`${styles.nav_item} ${selectedCategory === category ? styles.active : ""}`}
-                        onClick={() => handleCategoryChange(category)}
-                    >
-                        {category}
-                    </button>
+  const handleCategoryChange = (category) => {
+    setSelectedCategory(category);
+  };
+
+  const onClick = (event) => {
+    navigate(`/trigger/event/${event}`);
+  };
+
+  return (
+    <div className={styles.events_section} id="events">
+      <h1>EVENTS</h1>
+
+      <div className={styles.navbar}>
+        {categories.map((category) => (
+          <button
+            key={category}
+            className={`${styles.nav_item} ${
+              selectedCategory === category ? styles.active : ""
+            }`}
+            onClick={() => handleCategoryChange(category)}
+          >
+            {category}
+          </button>
+        ))}
+      </div>
+
+      <div className={styles.grid}>
+        {filteredEvents.map((event) => (
+          <div
+            key={event.id}
+            className={styles.card}
+            onClick={() => onClick(event.id)}
+          >
+            <div className={styles.body}>
+              <h2>{event.title}</h2>
+              <div className={styles.tags}>
+                {event.tags.map((tag) => (
+                  <span key={tag}>{tag}</span>
                 ))}
+              </div>
+              <b>
+                <p>{event.description}</p>
+              </b>
             </div>
-
-            <div className={styles.grid}>
-                {filteredEvents.map(event => (
-                    <div key={event.id} className={styles.card}>
-    
-                        <div className={styles.body}>
-                            <h2>{event.title}</h2>
-                            <div className={styles.tags}>
-                                {event.tags.map(tag => (
-                                    <span key={tag}>{tag}</span>
-                                ))}
-                            </div>
-                            <b><p>{event.description}</p></b>
-                        </div>
-                        <div className={styles.actions}>
-                            <button
-                                type="button"
-                                className={styles.button}
-                                onClick={() => window.open(event.registrationLink, "_blank")}
-                            >
-                                Register Now
-                            </button>
-                        </div>
-                    </div>
-                ))}
+            <div className={styles.actions}>
+              <button
+                type="button"
+                className={styles.button}
+                onClick={() => window.open(event.registrationLink, "_blank")}
+              >
+                Register Now
+              </button>
             </div>
-        </div>
-    );
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default EventsSection;
